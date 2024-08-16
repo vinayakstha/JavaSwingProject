0308e859bc8fd2522709ed31b49cc086d8bfd896
vinayakstha <vsth005@gmail.com>
2024-08-16 18:59:31 +0545

    project

---
+++ project/src/library/ConnectionProvider.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package library;
+import java.sql.*;
+
+/**
+ *
+ * @author Acer
+ */
+public class ConnectionProvider {
+    public static Connection getCon(){
+        Connection con = null;
+        try{
+//            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/libraryProject", "root", "password");
+            System.out.println("connected");
+            return con;
+        }catch(Exception e){
+            System.out.println(e);
+            return null;
+        }
+    }
+}
---
+++ project/src/project/Project.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project;
+
+/**
+ *
+ * @author Acer
+ */
+public class Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Login().setVisible(true);
+//        new library().setVisible(true);
+    }
+    
+}
